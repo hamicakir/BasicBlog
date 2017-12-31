04693a6f30e9f151fcff092a9589be15e1c21b94
hamicakir <hmi.cakir@gmail.com>
2017-12-31 15:57:19 +0300

    İmages delete

--- blog/panel/libraries/db.php
+++ blog/panel/libraries/db.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 
 try{
-    $db = new PDO("mysql:host=localhost;dbname=blog;charset=utf8", "root" , "");
+    $db = new PDO("mysql:host=localhost;dbname=multiext_blog;charset=utf8", "multiext_blog" , "17161514A+a");
 }catch (PDOException $exception){
     echo $exception->getMessage();
     die();
